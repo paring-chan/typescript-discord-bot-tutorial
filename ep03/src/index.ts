f2c4be09990e720c46d45511ec12c7e6156bbf5a
pikokr <parangee9706@gmail.com>
2021-05-19 09:44:49 +0900

    feat: load module

--- ep03/src/index.ts
+++ ep03/src/index.ts
@@ -1,9 +1,15 @@
 import {CommandClient} from "@pikostudio/command.ts";
+import fs from "fs";
+import path from "path";
 const config = require('../config.json')
 
 const client = new CommandClient({}, {
     owners: 'auto',
     prefix: config.prefix
+})
+
+fs.readdirSync(path.join(__dirname, 'modules')).forEach(x => {
+    client.registry.loadModule(path.join(__dirname, 'modules', x))
 })
 
 client.on('ready', () => console.log('Logged in as ' + client.user!.tag))
